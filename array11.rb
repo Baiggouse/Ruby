20a2555357ee1d9274f76d83144878fe9869d08c
Baig Gouse <baiggouse@BOM-MBP-19-4326.local>
2022-05-14 19:07:29 +0530

    first

--- array11.rb
+++ array11.rb
@@ -1,47 +1,28 @@
 def circle(r)
 
     result = 3.141592653 * r * r
-    
     return result
-    
     end
-    
-    
     
     
     print "Enter radius of circle: "
-    
     radius = gets.to_f
-    
     area = circle(radius)
-    
     puts "The area is #{area}."
     
     
-    
-    
     class Module
-    
     puts "enter the shape "
-    
     sha = gets
     
     
-    
     puts sha
-    
     if sha == "circle"
-    
     puts "x is greater than 2"
-    
     elsif sha == "square"
-    
     puts "x is 1"
     
     else
-    
     puts "I can't guess the number"
-    
     end
-    
     end
